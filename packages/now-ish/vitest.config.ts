fc9bacf078590ee356801eef79a834217444e2ce
xobotyi <xog3@yandex.ru>
2026-01-30 12:41:38 +0100

    chore: add vitest alias for core package

--- packages/now-ish/vitest.config.ts
+++ packages/now-ish/vitest.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
 	resolve: {
 		alias: {
+			'@ver0/now-ish': new URL('src/index.ts', import.meta.url).pathname,
 			'@ver0/now-ish_date-fns': new URL('../now-ish_date-fns/src/index.ts', import.meta.url).pathname,
 		},
 	},
